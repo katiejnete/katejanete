4311e4c2dc8629f72f497665fd912c136ca9bbdd
Kate Vong <katiejnete@gmail.com>
2024-12-13 17:10:25 -0800

    vite setup

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useContext } from "react";
+import { NavLink } from "react-router-dom";
+
+// import logo from "../assets/logo.png";
+// import geoIcon from "../assets/geoAltFill.svg";
+// import heartIcon from "../assets/heart.svg";
+// import "../stylesheets/NavBar.css";
+// import "../stylesheets/ModalForms.css";
+
+const NavBar = () => {
+
+  return (
+
+  );
+};
+
+export default NavBar;

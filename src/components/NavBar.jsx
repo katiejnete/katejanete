75290eeb6adeb27c5a68b352a9dacc6a19b5d089
Kate Vong <katiejnete@gmail.com>
2024-12-17 00:27:13 -0800

    update navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,41 @@
 import React, { useState, useContext } from "react";
 import { NavLink } from "react-router-dom";
 import logo from "../assets/logo.png";
+import hamburger from "../assets/hamburger.png";
 import "../stylesheets/NavBar.css";
 
 const NavBar = () => {
-    return (
-        <nav className="navbar">
-            <img className="navbar-logo" src={logo} alt="Kate Vong Logo" />
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/about">About Me</NavLink>
-            <NavLink to="/experience">Relevant Experience</NavLink>
-            <NavLink to="/projects">Projects</NavLink>
-            <NavLink to="/skills">Skills</NavLink>
-            <NavLink to="/contact">Contact</NavLink>
-            {/* <NavLink to="/playground">Playground</NavLink> */}
-        </nav>
-    );
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => setIsOpen(!isOpen);
+
+  return (
+    <nav className="navbar">
+      <img className="navbar-logo" src={logo} alt="Kate Vong Logo" />
+
+      <div className="navbar-hamburger" onClick={toggleMenu}>
+        <img src={hamburger} alt="" />
+      </div>
+
+      <ul className={`navbar-links ${isOpen ? "open" : ""}`}>
+        <li>
+          <NavLink to="/about">About Me</NavLink>
+        </li>
+        <li>
+          <NavLink to="/experience">Relevant Experience</NavLink>
+        </li>
+        <li>
+          <NavLink to="/projects">Projects</NavLink>{" "}
+        </li>
+        <li>
+          <NavLink to="/skills">Skills</NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact">Contact</NavLink>
+        </li>
+        {/* <NavLink to="/playground">Playground</NavLink> */}
+      </ul>
+    </nav>
+  );
 };
 
 export default NavBar;

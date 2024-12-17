74988639fa6361f6c0da28a0e79422832ab624ea
Kate Vong <katiejnete@gmail.com>
2024-12-16 22:55:53 -0800

    start navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,21 @@
 import React, { useState, useContext } from "react";
 import { NavLink } from "react-router-dom";
-
-// import logo from "../assets/logo.png";
-// import geoIcon from "../assets/geoAltFill.svg";
-// import heartIcon from "../assets/heart.svg";
-// import "../stylesheets/NavBar.css";
-// import "../stylesheets/ModalForms.css";
+import logo from "../assets/logo.png";
+import "../stylesheets/NavBar.css";
 
 const NavBar = () => {
-
-  return (
-
-  );
+    return (
+        <nav className="navbar">
+            <img className="navbar-logo" src={logo} alt="Kate Vong Logo" />
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/about">About Me</NavLink>
+            <NavLink to="/experience">Relevant Experience</NavLink>
+            <NavLink to="/projects">Projects</NavLink>
+            <NavLink to="/skills">Skills</NavLink>
+            <NavLink to="/contact">Contact</NavLink>
+            {/* <NavLink to="/playground">Playground</NavLink> */}
+        </nav>
+    );
 };
 
 export default NavBar;

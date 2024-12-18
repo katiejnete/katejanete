e90d939fc20de74afbbcf35ddf7dd2478bb2ccd2
Kate Vong <katiejnete@gmail.com>
2024-12-18 00:07:36 -0800

    add react hover effects to card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,16 +1,21 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import CardBody from "./CardBody";
 import CardText from "./CardText";
 import "../../stylesheets/Card.css";
 
-const Card = ({ title, text, cardClass, image }) => {
+const Card = ({ title, text, cardClass, firstImg, secondImg }) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const handleMouseEnter = () => setIsHovered(true);
+  const handleMouseLeave = () => setIsHovered(false);
+
   return (
     <div className={`card ${cardClass}`}>
-      <Link>
+      <Link onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
         <CardBody>
           <img
-            className="card-image"
-            src={image}
+            className={`card-image ${isHovered ? "hovered" : ""}`}
+            src={isHovered ? secondImg : firstImg}
             alt={`${cardClass} screenshot`}
           />
           <CardText title={title}>{text}</CardText>

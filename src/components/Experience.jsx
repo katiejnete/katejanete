58f4ab30ca46f2ebda508fb3f2396747cff08203
Kate Vong <katiejnete@gmail.com>
2024-12-18 22:40:33 -0800

    create and update experience page

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,30 @@
+import React, { useContext } from "react";
+import ExperienceContext from "../context/ExperienceContext";
+import "../stylesheets/Experience.css";
+
 const Experience = () => {
-    return (
-      <>
-        <h1>Relevant Experience</h1>
-      </>
-    );
-  };
-  
-  export default Experience;
+  const { experiences } = useContext(ExperienceContext);
+
+  return (
+    <div className="experience">
+      <h1>Relevant Experience</h1>
+      {experiences.map(({ role, company, timeline, skills }) => {
+        return (
+          <div key={role}>
+            <h2>
+              <strong>{role}</strong> - {company} <br />
+              <em>{timeline}</em>
+            </h2>
+            <ul>
+              {skills.map((skill, idx) => (
+                <li key={idx}>{skill}</li>
+              ))}
+            </ul>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Experience;

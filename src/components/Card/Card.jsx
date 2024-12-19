99411143732e99804b8f9ecd69fa72eb26cd846b
Kate Vong <katiejnete@gmail.com>
2024-12-18 17:28:44 -0800

    update to use images array

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,18 +4,19 @@ import CardBody from "./CardBody";
 import CardText from "./CardText";
 import "../../stylesheets/Card.css";
 
-const Card = ({ title, text, cardClass, firstImg, secondImg }) => {
+const Card = ({ project, cardClass }) => {
   const [isHovered, setIsHovered] = useState(false);
+  const {name, title, text, images} = project;
   const handleMouseEnter = () => setIsHovered(true);
   const handleMouseLeave = () => setIsHovered(false);
 
   return (
     <div className={`card ${cardClass}`}>
-      <Link onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      <Link to={`/projects/${name}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
         <CardBody>
           <img
             className={`card-image ${isHovered ? "hovered" : ""}`}
-            src={isHovered ? secondImg : firstImg}
+            src={isHovered ? images[1] : images[0]}
             alt={`${cardClass} screenshot`}
           />
           <CardText title={title}>{text}</CardText>

db58115327770df1a0d39e9bd053a9f1e834c542
Kate Vong <katiejnete@gmail.com>
2024-12-18 22:42:57 -0800

    make cards fill up screen more for smaller sized screens

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -9,7 +9,7 @@ const Projects = () => {
   return (
     <div className="projects">
     <h1>Projects</h1>
-      <div className="card-list projects">
+      <div className="card-list">
         {projects.map((project) => (
           <Card key={project.name} cardClass="projects" project={project} />
         ))}

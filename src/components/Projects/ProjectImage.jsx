2dd0f2d8b0c60c4324dd2295a4517114fc5f543d
Kate Vong <katiejnete@gmail.com>
2024-12-19 21:26:20 -0800

    create product image popup

---
+++ src/components/Projects/ProjectImage.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import "../../stylesheets/ProjectImage.css"
+
+const ProjectImage = ({ src, alt }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const openPopup = () => setIsOpen(true);
+  const closePopup = () => setIsOpen(false);
+
+  return (
+    <div>
+      <img className="project-image" onClick={openPopup} src={src} alt={alt} />
+
+      {isOpen && (
+        <div className="project-image-overlay">
+          <div className="project-image-box">
+            <button className="project-image-button" onClick={closePopup}>
+              &times;
+            </button>
+            <img
+              className={`project-popup-image ${isOpen ? "open" : ""}`}
+              src={src}
+              alt={alt}
+            />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ProjectImage;

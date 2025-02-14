98bc0bcf59c40935ec6901dc156a6cc29e544d6e
Kate Vong <katiejnete@gmail.com>
2025-02-13 21:55:10 -0800

    update about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,23 +8,21 @@ const About = () => {
       <div className="about-intro">
         <img src={profileImg} alt="" />
         <section>
-          <h2>
-            Introduction
-          </h2>
+          <h2>Introduction</h2>
           <p>
-            What if we could make every decision in life based on data,
-            insights, and a little bit of code?
-          </p>
-          <p>
-            Hello, I'm Kate! I'm a Software Engineer with a passion for turning
-            complex problems into user-friendly solutions.
+            Hello, I'm Kate! I'm a Software Engineer passionate about creating
+            dynamic web applications that incorporate AI. I'm excited to
+            continue learning more about AI and machine learning. During my free
+            time, I enjoy creating tech content to help beginner developers
+            start their journey. Teaching and giving back to the community is
+            something I will always love! What excites me most is the ability to
+            make a difference. The way technology can improve lives and
+            sustainability continues to motivate me!
           </p>
         </section>
       </div>
       <div className="about-journey">
-        <h2>
-          My Journey
-        </h2>
+        <h2>My Journey</h2>
         <p>
           While studying Mechanical Engineering, I realized that my true calling
           wasn't just working with hardware but it was using code to solve
@@ -34,9 +32,7 @@ const About = () => {
         </p>
       </div>
       <div className="passion">
-        <h2>
-          My Passion
-        </h2>
+        <h2>My Passion</h2>
         <p>
           In addition to building software, I was able to teach kids how to
           code. Managing classrooms of 1st and 5th graders, I saw how tools like
@@ -47,13 +43,12 @@ const About = () => {
         </p>
       </div>
       <div className="skills">
-        <h2>
-          Skills and Technical Expertise
-        </h2>
+        <h2>Skills and Technical Expertise</h2>
         <ul>
           <li>
             Programming Languages & Tools: Python, Node, JavaScript, Flask,
-            React (this portfolio is), PostgreSQL, Docker, Git, GitHub
+            React (this portfolio is made with React), PostgreSQL, TypeScript,
+            Git, GitHub, Docker
           </li>
           <li>
             Backend Development: Experience building robust backend systems,
@@ -62,18 +57,14 @@ const About = () => {
         </ul>
       </div>
       <div className="goals">
-        <h2>
-          Future Goals
-        </h2>
+        <h2>Future Goals</h2>
         <p>
           I'm excited to apply my background and skills to fields like AI and
           machine learning, where I can use data to make a difference.
         </p>
       </div>
       <div className="connect">
-        <h2>
-          Connect With Me
-        </h2>
+        <h2>Connect With Me</h2>
         <p>
           I'm excited to continue learning, growing, and attacking new
           challenges in software engineering. If you share the same passions,
